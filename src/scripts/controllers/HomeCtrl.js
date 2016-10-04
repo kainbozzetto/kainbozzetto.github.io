f1a60ddf63866f3f5ff1bdd326629a59c00b38aa
unknown <kainbozzetto@gmail.com>
2016-10-04 17:25:16 +1100

    Updated header content

--- src/scripts/controllers/HomeCtrl.js
+++ src/scripts/controllers/HomeCtrl.js
@@ -1,7 +1,11 @@
 module.exports = ['$scope', '$http', function($scope, $http) {
   $scope.message = 'Welcome!';
 
-  $http.get('https://api.github.com/users/kainbozzetto/repos')
+  $http.get('https://api.github.com/users/kainbozzetto/repos', {
+    headers: {
+      'Access-Control-Allow-Origin': *
+    }
+  })
     .then(function(data) {
       $scope.gits = data.data;
     }, function(error) {
