5021aaa96defc3cb15460e9a3cb288c931e99bbf
unknown <kainbozzetto@gmail.com>
2016-10-04 17:18:53 +1100

    Basic template completed

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,20 @@ var browserify = require('browserify');
 var uglify = require('gulp-uglify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
+var sass = require('gulp-ruby-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var cssnano = require('gulp-cssnano');
+var rename = require('gulp-rename');
 
 gulp.task('styles', function() {
   gulp.src('./node_modules/bootstrap/dist/css/bootstrap.min.css')
+    .pipe(gulp.dest('./dist/styles'));
+
+  return sass('./src/styles/main.scss', { style: 'expanded' })
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(gulp.dest('./dist/styles'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(cssnano())
     .pipe(gulp.dest('./dist/styles'));
 });
 
@@ -19,9 +30,6 @@ gulp.task('browserify', function() {
 });
 
 gulp.task('views', function() {
-  gulp.src('./src/index.html')
-    .pipe(gulp.dest('./dist'));
-
   gulp.src('./src/views/**/*.html')
     .pipe(gulp.dest('./dist/views'));
 })
--- src/scripts/controllers/HomeCtrl.js
+++ src/scripts/controllers/HomeCtrl.js
@@ -3,7 +3,7 @@ module.exports = ['$scope', '$http', function($scope, $http) {
 
   $http.get('https://api.github.com/users/kainbozzetto/repos')
     .then(function(data) {
-      console.log(data);
+      $scope.gits = data.data;
     }, function(error) {
       console.log(error);
     });
