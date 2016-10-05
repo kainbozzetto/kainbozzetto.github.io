61d35475af14d4e5ea5ca09859b7719130f62b69
unknown <kainbozzetto@gmail.com>
2016-10-05 15:00:51 +1100

    minor updates to js

--- src/scripts/controllers/HomeCtrl.js
+++ src/scripts/controllers/HomeCtrl.js
@@ -1,11 +1,6 @@
 module.exports = ['$scope', '$http', function($scope, $http) {
-  $scope.message = 'Welcome!';
 
-  $http.get('https://api.github.com/users/kainbozzetto/repos', {
-    headers: {
-      //'Access-Control-Allow-Origin': '*'
-    }
-  })
+  $http.get('https://api.github.com/users/kainbozzetto/repos')
     .then(function(data) {
       $scope.gits = data.data;
     }, function(error) {
